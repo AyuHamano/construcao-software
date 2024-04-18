29449b3d8b232fa83bf926a9c712bf2dca59de1e
mateus <mateus@teste.com.br>
2024-04-18 20:14:43 -0300

    Criação das clases Pais, cidade e estado

---
+++ endereco/src/main/java/org/example/endereco/Cidade.java
@@ -0,0 +1,31 @@
+package org.example.endereco;
+
+public class Cidade {
+    private String nome;
+    private String cep;
+    private Estado estado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
