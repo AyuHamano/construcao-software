871afda14a2857752785a134662c2ad74cdfcfb7
mateus <mateus@teste.com.br>
2024-04-18 20:15:41 -0300

    Criação das clases Pais, cidade e estado

---
+++ endereco/src/main/java/org/example/endereco/Bairro.java
@@ -0,0 +1,31 @@
+package org.example.endereco;
+
+public class Bairro {
+    private String nome;
+    private String cep;
+    private Cidade cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+}
---
+++ endereco/src/main/java/org/example/endereco/Logradouro.java
@@ -0,0 +1,24 @@
+package org.example.endereco;
+
+import org.example.enums.TipoLogradouro;
+
+public class Logradouro {
+    private String nome;
+    private TipoLogradouro tipoLogradouro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoLogradouro getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(TipoLogradouro tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+}
